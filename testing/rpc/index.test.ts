b2b46a3d2f5bb7df35535e2b13e61b715a7bd50c
Prateek Rathod <prateek@zondax.ch>
2022-07-05 00:02:10 +0530

    chore: add create actor test

--- testing/rpc/index.test.ts
+++ testing/rpc/index.test.ts
@@ -7,9 +7,9 @@ import {
 import FilecoinRPC from "@zondax/filecoin-signing-tools/rpc";
 
 import fs from "fs";
-import log4js from "log4js";
 import path from "path";
 import * as cbor from "@ipld/dag-cbor";
+import { getFee, logger } from "./utils";
 
 jest.setTimeout(300 * 1000);
 
@@ -22,24 +22,21 @@ const INIT_ACTOR_ADDRESS = "f01";
 const INIT_ACTOR_INSTALL_METHOD = 3;
 const INIT_ACTOR_CREATE_METHOD = 2;
 
-const logger = log4js.getLogger();
-logger.level = process.env["LOG_LEVEL"] || "TRACE";
-
-let seed;
-let keys;
-let actorCid;
-let instanceAddress;
+let Seed;
+let Keys;
+let ActorCid;
+let InstanceAddress;
 
 beforeAll(() => {
-  seed = SEED || generateMnemonic();
-  logger.trace(`Seed: [${seed}]`);
+  Seed = SEED || generateMnemonic();
+  logger.trace(`Seed: [${Seed}]`);
 
-  keys = keyDerive(seed, "m/44'/461'/0/0/1", "");
-  logger.trace(`Address: ${keys.address}`);
+  Keys = keyDerive(Seed, "m/44'/461'/0/0/1", "");
+  logger.trace(`Address: ${Keys.address}`);
 
   logger.trace(
     `Key file to add address on lotus devnet node: ${Buffer.from(
-      `{"Type":"secp256k1","PrivateKey":"${keys.private_base64}"}`
+      `{"Type":"secp256k1","PrivateKey":"${Keys.private_base64}"}`
     ).toString("hex")}`
   );
 });
@@ -53,14 +50,12 @@ test("Install actor", async () => {
   const params = cbor.encode([new Uint8Array(code.buffer)]);
   logger.trace("Params encoded");
 
-  console.log(FilecoinRPC)
-
   const filRPC = new FilecoinRPC({ url: URL, token: TOKEN });
-  const nonce = (await filRPC.getNonce(keys.address)).result;
+  const nonce = (await filRPC.getNonce(Keys.address)).result;
   logger.trace(`Nonce: ${nonce}`);
 
   let tx = {
-    From: keys.address,
+    From: Keys.address,
     To: INIT_ACTOR_ADDRESS,
     Value: "0",
     Method: INIT_ACTOR_INSTALL_METHOD,
@@ -74,9 +69,9 @@ test("Install actor", async () => {
   tx = await getFee(filRPC, tx);
   if (!tx) return;
 
-  const signedTx = transactionSign(tx, keys.private_base64);
+  const signedTx = transactionSign(tx, Keys.private_base64);
 
-  let sentTx  
+  let sentTx;
   try {
     sentTx = await filRPC.sendSignedMessage({
       Message: tx,
@@ -84,10 +79,9 @@ test("Install actor", async () => {
     });
     logger.trace(`Sent tx response: ${JSON.stringify(sentTx)}`);
   } catch (err) {
-    console.log(err.response)
-    throw new Error("failed")
+    console.log(err.response);
+    throw new Error("failed");
   }
-
 
   expect(sentTx.result).toBeDefined();
   expect(sentTx.result.Receipt).toBeDefined();
@@ -109,33 +103,78 @@ test("Install actor", async () => {
 
     logger.info(`CID: ${cid.toString()}`);
     logger.info(`Is installed: ${isInstalled}`);
-    actorCid = cid;
+    ActorCid = cid;
   }
 });
 
-async function getFee(filRPC, tx) {
-  try {
-    const fees = await filRPC.getGasEstimation({ ...tx });
-    logger.trace(`Fees: ${JSON.stringify(fees)}`);
+test("Create Actor", async () => {
+  ActorCid = "bafk2bzacebc4i3jul2vgavroqf43q4t4htgm2n4o6aayu4wckz2uhxcqiikku";
+  expect(ActorCid).toBeDefined();
 
-    expect(fees.error).not.toBeDefined();
-    if (fees.error) return;
+  logger.info(`Instantiating Actor [${ActorCid.toString()}]`);
 
-    expect(fees.result).toBeDefined();
-    expect(fees.result.GasFeeCap).toBeDefined();
-    expect(fees.result.GasPremium).toBeDefined();
-    expect(fees.result.GasLimit).toBeDefined();
+  const params = cbor.encode([
+    ActorCid,
+    Buffer.from(
+      cbor.encode(["Zondax Coin", "ZDX", 18, 1000000, "100"])
+    ).toString("base64"),
+  ]);
+  logger.trace("Params encoded");
 
-    const { GasFeeCap, GasPremium, GasLimit } = fees.result;
-    tx = {
-      ...tx,
-      GasFeeCap,
-      GasPremium,
-      GasLimit,
-    };
+  const filRPC = new FilecoinRPC({ url: URL, token: TOKEN });
+  const nonce = (await filRPC.getNonce(Keys.address)).result;
+  logger.trace(`Nonce: ${nonce}`);
 
-    return tx;
-  } catch (err) {
-    logger.error(`Error fetching fees: ${JSON.stringify(err)}`);
+  let tx = {
+    From: Keys.address,
+    To: INIT_ACTOR_ADDRESS,
+    Value: "0",
+    Method: INIT_ACTOR_CREATE_METHOD,
+    Params: Buffer.from(params).toString("base64"),
+    Nonce: nonce,
+    GasFeeCap: "0",
+    GasPremium: "0",
+    GasLimit: 0,
+  };
+
+  tx = await getFee(filRPC, tx);
+  if (!tx) return;
+
+  const signedTx = transactionSign(tx, Keys.private_base64);
+
+  const sentTx = await filRPC.sendSignedMessage({
+    Message: tx,
+    Signature: signedTx.Signature,
+  });
+  logger.trace(`Sent tx response: ${JSON.stringify(sentTx)}`);
+
+  expect(sentTx.result).toBeDefined();
+  expect(sentTx.result.Receipt).toBeDefined();
+  expect(sentTx.result.Receipt.ExitCode).toBe(0);
+
+  if (sentTx.result.Receipt.ExitCode == 0) {
+    let idAddr, robustAddr;
+    if (sentTx.result.ReturnDec) {
+      idAddr = sentTx.result.ReturnDec.IDAddress;
+      robustAddr = sentTx.result.ReturnDec.RobustAddress;
+    } else {
+      const respBuffer = Buffer.from(sentTx.result.Receipt.Return, "base64");
+
+      let arrayBuffer = new ArrayBuffer(respBuffer.length);
+      let typedArray = new Uint8Array(arrayBuffer);
+      for (let i = 0; i < respBuffer.length; ++i) {
+        typedArray[i] = respBuffer[i];
+      }
+
+      [idAddr, robustAddr] = cbor.decode(typedArray);
+    }
+
+    expect(idAddr).toBeDefined();
+    expect(robustAddr).toBeDefined();
+
+    logger.info(`ID Address: ${idAddr.toString()}`);
+    logger.info(`Robust address: ${robustAddr.toString()}`);
+
+    InstanceAddress = idAddr.toString();
   }
-}
+});
